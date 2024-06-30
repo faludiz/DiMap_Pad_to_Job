86a9667c036fd98b7a93a1f7d449a9a5b1207517
faludiz <zoltan@faludi.info>
2024-06-30 09:13:47 +0200

    some refactorings

--- dimappad2job.py
+++ dimappad2job.py
@@ -47,23 +47,24 @@ def rad_dms(rad):
     deg, mnt = divmod(mnt, 60)
     return mult * (deg + mnt / 100 + sec / 10000)
 
+
 def get_ts_data(info):
     """Extract TS information from the given data."""
-    deviceModel = ""
-    deviceSn = ""
+    device_model = ""
+    device_sn = ""
 
     for i in range(len(info)):
         sdata = info[i].split(':')
         if len(sdata) > 1:
             if sdata[0] == "deviceModel":
-                deviceModel = sdata[1]
+                device_model = sdata[1]
             if sdata[0] == "deviceSn":
-                deviceSn = sdata[1]
-    return [deviceModel, deviceSn]
+                device_sn = sdata[1]
+    return [device_model, device_sn]
+
 
 def get_station_data(info):
     """Extract station information from the given data."""
-    stn = []
     name = ""
     hi = ""
     for i in range(len(info)):
@@ -73,14 +74,11 @@ def get_station_data(info):
                 name = sdata[1]
             if sdata[0] == "hi":
                 hi = sdata[1]
-    stn.append(name)  #0
-    stn.append(hi)  #1
-    return stn
+    return [name, hi]
 
 
 def get_observation_data(info):
     """Extract observation information from the given data."""
-    obs = []
     ha = 0.0
     va = 0.0
     sd = 0.0
@@ -96,29 +94,24 @@ def get_observation_data(info):
                 va = rad_dms(float(sdata[1]))
             if sdata[0] == "sd":
                 sd = float(sdata[1])
-    obs.append(ha)  #0
-    obs.append(va)  #1
-    obs.append(sd)  #2
-    obs.append(hr)  #3
-    return obs
+    return [ha, va, sd, hr]
 
 
 def save_job(folder, projname):
     """Save the job information to a file."""
-    WorkID = get_work_id(folder, projname)
+    work_id = get_work_id(folder, projname)
     con = sqlite3.connect(folder + "/" + "surveyDatabase.db")
     cur = con.cursor()
-    sql = 'SELECT * FROM "Point" WHERE "work_id" IS "' + WorkID + '";'
+    sql = 'SELECT * FROM "Point" WHERE "work_id" IS "' + work_id + '";'
 
-    StationName = ""
-    LastStationName = ""
-    jobfn = projname + '.job'
+    station_name = ""
+    last_station_name = ""
 
-    job = open(jobfn, "w")
+    job = open(projname + ".job", "w")
 
     # header
     job.write("23=4112\n")
-    job.write("50=%s\n" % (projname))
+    job.write("50=%s\n" % projname)
 
     for row in cur.execute(sql):
         pname = row[0]
@@ -136,26 +129,31 @@ def save_job(folder, projname):
             sdata = sinfo[i].split(':')
             if len(sdata) > 1:
                 if sdata[0] == "stationName":
-                    StationName = sdata[1]
-                    if StationName != LastStationName:
+                    station_name = sdata[1]
+                    if station_name != last_station_name:
                         nfo = get_ts_data(sinfo)
                         job.write("55=%s %s\n" % (nfo[0], nfo[1]))
                         nfo = get_station_data(sinfo)
-                        job.write("2=%s\n" % (nfo[0]))
-                        job.write("3=%s\n" % (nfo[1]))
-                        LastStationName = StationName
-        ## check if TS data is present
+                        job.write("2=%s\n" % nfo[0])
+                        job.write("3=%s\n" % nfo[1])
+                        last_station_name = station_name
+        # check if TS data is present
         if "TS:deviceType" in pinfo:
             nfo = get_observation_data(sinfo)
-            job.write("5=%s\n" % (pname))  # pname
-            job.write("4=%s\n" % (pcode))  # pcode
+            job.write("5=%s\n" % pname)  # pname
+            job.write("4=%s\n" % pcode)  # pcode
             prefix = ""
+            """
+                F1  F2
+            HA  7   17
+            ZA  8   18            
+            """
             if nfo[1] > 180:
                 prefix = "1"
-            job.write(prefix + "7=%0.5f\n" % (nfo[0]))  # ha
-            job.write(prefix + "8=%0.5f\n" % (nfo[1]))  # va
-            job.write("9=%0.4f\n" % (nfo[2]))  # sd
-            job.write("6=%0.4f\n" % (nfo[3]))  # rh
+            job.write(prefix + "7=%0.5f\n" % nfo[0])  # ha
+            job.write(prefix + "8=%0.5f\n" % nfo[1])  # va
+            job.write("9=%0.4f\n" % nfo[2])  # sd
+            job.write("6=%0.4f\n" % nfo[3])  # rh
     con.close()
     job.close()
 
@@ -163,7 +161,7 @@ def save_job(folder, projname):
 def main():
     tmpfolder = tempfile.gettempdir() + "/" + uuid.uuid4().hex
 
-    if ((len(sys.argv) <= 2)):
+    if len(sys.argv) <= 2:
         print("DiMap Pad Backup to Job © Faludi Zoltán, INTELLIGEO Kft")
         print("")
         print("Usage:")
@@ -177,7 +175,7 @@ def main():
         print("dimappad2job.py --list exported_backup_file.zip")
         sys.exit(1)
 
-    if ((len(sys.argv) >= 2)) and (sys.argv[1] == "--list"):
+    if (len(sys.argv) >= 2) and (sys.argv[1] == "--list"):
         zipfn = sys.argv[2]
         if os.path.exists(zipfn):
             unzip_file(zipfn, tmpfolder)
@@ -187,7 +185,7 @@ def main():
             print('list.done')
         sys.exit(0)
 
-    if (len(sys.argv) >= 3):
+    if len(sys.argv) >= 3:
         zipfn = sys.argv[1]
         projname = sys.argv[2]
 
